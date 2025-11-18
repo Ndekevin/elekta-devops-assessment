6040f1d7b519f0d933bd8d4f7f305b6eb10228de
Ndekevin <ndekevin97@icloud.com>
2025-11-18 17:32:16 +0000

    Nov 18, 2025, 9:32 AM

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,84 +1,84 @@
 resource "azurerm_resource_group" "main" {
-    name = var.resource_group_name
-    location = var.location
-    tags = {
-        Environment =  var.environment
-        ManagedBy  = "Terraform"
-    }
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
 }
 
 resource "azurerm_virtual_network" "main" {
-    name                = "vnet-${var.resource_group_name}"
-    address_space       = [var.vnet_address_space]
-    location            = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
-    tags = {
-        Environment =  var.environment
-        ManagedBy  = "Terraform"
-    }
+  name                = "vnet-${var.resource_group_name}"
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
 }
 
 #----------------------subnet configuration-----------------------------
 
 resource "azurerm_subnet" "internal" {
-    name = "subnet-${var.environment}"
-    resource_group_name = azurerm_resource_group.main.name
-    virtual_network_name = azurerm_virtual_network.main.name
-    address_prefixes = [var.subnet_address_prefix]
+  name                 = "subnet-${var.environment}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.subnet_address_prefix]
 }
 
 #----------------------Network Security Group-----------------------------
 
 resource "azurerm_network_security_group" "main" {
-    name = "nsg-${var.environment}"
-    location = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
+  name                = "nsg-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
-    # Allow RDP from anywhere (consider restricting in production)
+  # Allow RDP from anywhere (consider restricting in production)
 
-    security_rule {
-        name = "Allow-RDP"
-        priority = 100
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "3389"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-    # Allow communication between VMs on all ports
+  # Allow communication between VMs on all ports
 
-    security_rule {
-        name = "AllowInternalTraffic"
-        priority = 101
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "*"
-        source_port_range = "*"
-        destination_port_range = "*"
-        source_address_prefix = var.subnet_address_prefix
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "AllowInternalTraffic"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = var.subnet_address_prefix
+    destination_address_prefix = "*"
+  }
 
-    # Allow outbound internet traffic
-    security_rule {
-        name                       = "AllowOutbound"
-        priority                   = 100
-        direction                  = "Outbound"
-        access                     = "Allow"
-        protocol                   = "*"
-        source_port_range          = "*"
-        destination_port_range     = "*"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  # Allow outbound internet traffic
+  security_rule {
+    name                       = "AllowOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-    tags = {
-        Environment = var.environment
-        ManagedBy   = "Terraform"
-    }
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
 
 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,56 +1,56 @@
-variable azure_subscription_id {
-    description = "The Subscription ID for the Azure account."
-    type        = string
-    sensitive = true
+variable "azure_subscription_id" {
+  description = "The Subscription ID for the Azure account."
+  type        = string
+  sensitive   = true
 }
 
 variable "resource_group_name" {
-    description = "Name of the Azure Resource Group"
-    type = string
-    default = "elekta-devops-test"
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "elekta-devops-test"
 }
 
 variable "location" {
-    description = "Azure region for resources"
-    type        = string
-    default     = "eastus"
+  description = "Azure region for resources"
+  type        = string
+  default     = "eastus"
 }
 
 variable "vnet_address_space" {
-    description = " Address prefix for Subnet"
-    type = string
-    default = "10.0.0.0/16"
+  description = " Address prefix for Subnet"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_address_prefix" {
-    description = " Address prefix for Subnet"
-    type = string
-    default = "10.0.1.0/24"
+  description = " Address prefix for Subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "vm_size" {
-    description = "Size of the Virtual Machine"
-    type = string
-    default = "Standard_B2s"
+  description = "Size of the Virtual Machine"
+  type        = string
+  default     = "Standard_B2s"
 }
 
 variable "admin_username" {
-    description = "Admin username for VMs"
-    type = string
-    default = "Elekta"
-    sensitive = true
+  description = "Admin username for VMs"
+  type        = string
+  default     = "Elekta"
+  sensitive   = true
 }
 
 variable "admin_password" {
-    description = "Admin password for VMs"
-    type = string
-    sensitive = true
-    # This should come from terraform.tfvars or environment variables
+  description = "Admin password for VMs"
+  type        = string
+  sensitive   = true
+  # This should come from terraform.tfvars or environment variables
 }
 
 variable "environment" {
-    description = "Deployment environment"
-    type        = string
-    default     = "prod"
+  description = "Deployment environment"
+  type        = string
+  default     = "prod"
 }
 
